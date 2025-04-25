153b753c2f66ebe3e66ef1d3e2de379c1a99c642
Marcos <marcos.2006.gs@gmail.com>
2025-04-25 10:15:35 +0200

    Modificacion de la VentanaPrincipalMonitor

--- PROGRAMACION/ProyectoIntegrador/src/vistas/VentanaPrincipalMonitor.java
+++ PROGRAMACION/ProyectoIntegrador/src/vistas/VentanaPrincipalMonitor.java
@@ -4,55 +4,63 @@
 package vistas;
 
 import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.Color;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-/**
- * 
- */
 public class VentanaPrincipalMonitor extends JFrame {
 	
-	public VentanaPrincipalMonitor() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(600,600);
-		setLocationRelativeTo(null);  //Centra la ventana
+	
+	public VentanaPrincipalMonitor()
+	{
+		setFont(new Font("Dialog", Font.BOLD, 12));
+		setTitle("Mis Actividades (Monitor)");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 571, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 376, Short.MAX_VALUE)
+		);
+		getContentPane().setLayout(groupLayout);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu mnNewMenu = new JMenu("Actividades");
-		mnNewMenu.setBackground(new Color(255, 255, 255));
-		menuBar.add(mnNewMenu);
+		JMenu mnActividades = new JMenu("Actividades");
+		menuBar.add(mnActividades);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Nueva actividad");
-		mnNewMenu.add(mntmNewMenuItem);
+		JMenuItem mntmNewMenuItem = new JMenuItem("Nueva Actividad");
+		mnActividades.add(mntmNewMenuItem);
 		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Ver todas las actividades");
-		mnNewMenu.add(mntmNewMenuItem_1);
+		JMenuItem mntmVerActividadesDisponibles = new JMenuItem("Ver todas las actividades");
+		mnActividades.add(mntmVerActividadesDisponibles);
 		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Mis actividades");
-		mnNewMenu.add(mntmNewMenuItem_2);
+		JMenuItem mntmMisActividades = new JMenuItem("Mis actividades");
+		mnActividades.add(mntmMisActividades);
 		
-		JButton btnNewButton = new JButton("Salas");
-		btnNewButton.setBackground(new Color(255, 255, 255));
-		menuBar.add(btnNewButton);
+		JMenu mnSalas = new JMenu("Salas");
+		menuBar.add(mnSalas);
 		
-		JButton btnNewButton_1 = new JButton("Datos Personales");
-		menuBar.add(btnNewButton_1);
+		JMenu mnNewMenu_2 = new JMenu("Datos Personales");
+		menuBar.add(mnNewMenu_2);
 		
 		
 		
+		}
+}
+
 		
 		
-		
-		
-	}
-	
-	
-	public void hacerVisible() {
-		setVisible(true);
-	}
-	
+
 	
 	
 
-}
+
